55eba0be89c508ee8e16266a699b643e64ec9202
Sohan Suralkar <88510230+Sohansuralkar@users.noreply.github.com>
2025-05-30 01:03:48 +0530

    Add files via upload

---
+++ inheritance3.py
@@ -0,0 +1,20 @@
+#dumy code for multilevel inheritance
+class Demo:
+    pass
+
+class A(Demo):
+    pass
+
+
+
+class B(A):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
+    main()
